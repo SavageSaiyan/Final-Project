31d8763f19061222bb00adf1b7d568b56321f392
Sterling <sterlingduncan9@gmail.com>
2023-09-09 17:53:09 -0400

    finished the login functionality

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,5 +1,7 @@
  import React, { useState } from 'react'
  import axios from "axios"
+ import {useCookies} from 'react-cookie'
+ import {useNavigate} from 'react-router-dom'
  
  function Auth() {
    return (
@@ -14,6 +16,26 @@
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
 
+  const [_, setCookies] = useCookies(["access_token"])
+
+  const navigate = useNavigate();
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:3001/auth/login", {
+        username,
+        password,
+      });
+      //set response we get back to our cookie and store userID inside local storage 
+      setCookies("access_token", response.data.token);
+      window.localStorage.setItem("userID", response.data.userID);
+      navigate("/")
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
 // return destructured form
   return (
   
@@ -23,6 +45,7 @@
     password={password} 
     setPassword={setPassword}
     label= "Login"
+    onSubmit={onSubmit}
     />
   )     
 };
