c448e21afea2b9f691f3ecc33a08fad036129cc2
Sterling <sterlingduncan9@gmail.com>
2023-09-10 14:56:32 -0400

    fixed add ingredient button. adds input field

--- client/src/pages/CreateRecipe.js
+++ client/src/pages/CreateRecipe.js
@@ -1,7 +1,6 @@
- import React, { useState } from 'react'
+import React, { useState } from "react";
 
- 
- function CreateRecipe() {
+function CreateRecipe() {
   const [recipe, setRecipe] = useState({
     name: "",
     ingredients: [],
@@ -12,68 +11,70 @@
   });
 
   const handleChange = (event) => {
-    const {name, value} = event.target;
-    setRecipe({...recipe, [name]: value});
+    const { name, value } = event.target;
+    setRecipe({ ...recipe, [name]: value });
   };
 
-const handleIngredientChange = (event, idx) => {
-    const {value} = event.target;
+  const handleIngredientChange = (event, idx) => {
+    const { value } = event.target;
     const ingredients = recipe.ingredients;
     ingredients[idx] = value;
-    setRecipe({...recipe, ingredients});
-    console.log(recipe)
+    setRecipe({ ...recipe, ingredients });
+    console.log(recipe);
   };
-//set recipe object to be the same as it was before , but change the ingredients field
+  //set recipe object to be the same as it was before , but change the ingredients field
   const addIngredient = () => {
-    setRecipe({...recipe, ingredients: [...recipe.ingredients, ""]})
-  }
-  console.log(recipe)
-   return (
-     <div className='create-recipe'> 
-     <h2>Create Recipe</h2>
-     <form>
-      <label htmlFor='name'> Name</label>
-      <input type='text' id='name' name='name' onChange={handleChange} />
-      <label htmlFor='ingredients'>Ingredients</label>
-      {recipe.ingredients.map((ingredient, idx) => {
-        <input 
-        key={idx} 
-        type='text' 
-        name='ingredients' 
-        value={ingredient} 
-        onChange={(event) => handleIngredientChange(event, idx )}
+    setRecipe({ ...recipe, ingredients: [...recipe.ingredients, ""] });
+  };
+  console.log(recipe);
+  return (
+    <div className="create-recipe">
+      <h2>Create Recipe</h2>
+      <form>
+        <label htmlFor="name"> Name</label>
+        <input type="text" id="name" name="name" onChange={handleChange} />
+        <label htmlFor="ingredients">Ingredients</label>
+{recipe.ingredients.map((ingredient, idx) => (
+  <input
+    key={idx}
+    type="text"
+    name="ingredients"
+    value={ingredient}
+    onChange={(event) => handleIngredientChange(event, idx)}
+  />
+))}
+<button onClick={addIngredient} type="button">
+  Add Ingredient
+</button>
+
+        <label htmlFor="instructions">Instructions</label>
+        <textarea
+          id="instructions"
+          name="instructions"
+          onChange={handleChange}
+        ></textarea>
+        <label htmlFor="imageUrl">Image URL</label>
+        <input
+          type="text"
+          id="imageUrl"
+          name="imageUrl"
+          onChange={handleChange}
         />
-      })}
-      <button onClick={addIngredient} type='button'>
-        Add Ingredient
-        </button>
-      <label htmlFor='instructions'>Instructions</label>
-      <textarea 
-      id='instructions' 
-      name='instructions'
-      onChange={handleChange}></textarea>
-      <label htmlFor='imageUrl'>Image URL</label>
-      <input 
-      type='text' 
-      id='imageUrl' 
-      name='imageUrl' 
-      onChange={handleChange}
-      />
-      <label htmlFor='cookingTime'>CookingTime (minutes)</label>
-      <input 
-      type='number' 
-      id='cookingTime' 
-      name='cookingTime' 
-      onChange={handleChange}
-      />
-      <button type='submit'>Create Recipe</button>
-    </form> 
-     </div>
-   )
- }
- 
- export default CreateRecipe
- 
+        <label htmlFor="cookingTime">CookingTime (minutes)</label>
+        <input
+          type="number"
+          id="cookingTime"
+          name="cookingTime"
+          onChange={handleChange}
+        />
+        <button type="submit">Create Recipe</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateRecipe;
+
 //  const CreateRecipe = () => {
 //     return <div>CreteRecipe</div>;
 // };
